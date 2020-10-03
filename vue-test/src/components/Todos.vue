fbd84e3cc717fb1839ccba2333c7ce78ee94fe0c
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2020-10-03 21:12:45 +0600

    Add files via upload

---
+++ vue-test/src/components/Todos.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="">
+    <div v-for="todo in todos" :key="todo.id">
+      <TodoItem v-bind:todo='todo' @del-todo="$emit('del-todo', todo.id)"/>
+    </div>
+  </div>
+</template>
+
+<script>
+  import TodoItem from './TodoItems';
+
+  export default{
+    name: 'Todos',
+    props: ['todos'],
+    components: {
+      TodoItem
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
