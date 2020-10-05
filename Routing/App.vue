d15fa1b0ac884b5e2b642a1ff107661e5c4541f5
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2020-10-05 20:11:28 +0600

    Create App.vue

---
+++ Routing/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app" class="center">
+    <h1 class="display-1">Vue Routing Example</h1>
+    <a href="/home" class="btn btn-primary mr-3">Home</a>
+    <a href="/user" class="btn btn-info">User</a>
+    <hr>
+    <router-view />
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  *{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body{
+    font-family: Arial, Helvetica, san-serif;
+    line-height: 1.4;
+  }
+
+  .center{
+    text-align: center;
+  }
+</style>
