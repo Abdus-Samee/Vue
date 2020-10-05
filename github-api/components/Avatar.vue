43318b8d21bc4f2882521a72b1dcb11cc193594f
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2020-10-05 19:43:01 +0600

    Create Avatar.vue

---
+++ github-api/components/Avatar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <img class="img-fluid" v-bind:src="image" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Avatar",
+    props: ['image']
+}
+</script>
+
+<style scoped>
+    img{
+        border-radius: 50%;
+        height: 20vh;
+        width: 20vw;
+        object-fit: contain;
+    }
+</style>
