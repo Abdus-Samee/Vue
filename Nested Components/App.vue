ca7ceb43351a13cfa97a5ab22ee867d7aea46e3d
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2020-10-06 18:38:47 +0600

    Create App.vue

---
+++ Nested Components/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Header></Header>
+    <Ninjas></Ninjas>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Ninjas from './components/Ninjas';
+
+export default {
+  name: 'App',
+  components: {
+    Header, Footer, Ninjas
+  }
+}
+
+</script>
+
+<style>
+  
+</style>
